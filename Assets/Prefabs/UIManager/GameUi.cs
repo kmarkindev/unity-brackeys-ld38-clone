195b19cc2733aa27f63932ee0a45ec4ec0c9d45f
Kirill <kmarkin@mbsicorp.com>
2021-04-09 13:04:08 +0300

    save

---
+++ Assets/Prefabs/UIManager/GameUi.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class GameUi : MonoBehaviour
+{
+    private GameManager gameManager;
+    public TMP_Text score;
+
+    private void Awake()
+    {
+        gameManager = FindObjectOfType<GameManager>();
+    }
+
+    private void Update()
+    {
+        score.text = gameManager.CurrentScore.ToString("0.00s");
+    }
+
+}
---
+++ Assets/Prefabs/UIManager/UIManager.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    private GameManager gameManager;
+
+    public GameObject startMenuUi;
+    public GameObject gameUi;
+    public GameObject gameOverUi;
+
+    private void Awake()
+    {
+        gameManager = FindObjectOfType<GameManager>();
+
+        gameManager.OnGameStart.AddListener(() =>
+        {
+            HideGameOverUi();
+            HideStartMenuUi();
+            ShowGameUi();
+        });
+
+        gameManager.OnGameOver.AddListener(() =>
+        {
+            HideGameUi();
+            ShowGameOverUi();
+        });
+
+        gameManager.OnStartMenu.AddListener(() =>
+        {
+            HideGameUi();
+            HideGameOverUi();
+            ShowStartMenuUi();
+        });
+
+    }
+
+    public void ShowStartMenuUi()
+    {
+        startMenuUi.SetActive(true);
+    }
+
+    public void HideStartMenuUi()
+    {
+        startMenuUi.SetActive(false);
+    }
+
+    public void ShowGameUi()
+    {
+        gameUi.SetActive(true);
+    }
+
+    public void HideGameUi()
+    {
+        gameUi.SetActive(false);
+    }
+
+    public void ShowGameOverUi()
+    {
+        gameOverUi.SetActive(true);
+    }
+
+    public void HideGameOverUi()
+    {
+        gameOverUi.SetActive(false);
+    }
+
+}
